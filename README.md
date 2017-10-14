6ae42be4f67ae9bdcb377d54a38ae0623367c02a
Roman Himadinov <roman@brandetize.com>
2017-10-14 14:31:12 -0700

    Added the project

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Recaptcha
-Implemented Google's reCaptcha v2 in a client-server environment with Node.js. &lt;br> Example: submit the client side form to the server using the Fetch API. 
+Implemented Google's reCaptcha v2 in a client-server environment with Node.js.
+Example: submit the client side form to the server using the Fetch API. 
+
+### Version
+1.0.0
+
+## Usage
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To run
+
+```sh
+$ npm start
+```
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/subscribe', (req, res) => {
+  if (
+    req.body.captcha === undefined ||
+    req.body.captcha === '' ||
+    req.body.captcha === null
+  ) {
+    return res.json({"success": false, "msg": "Please select captcha"});
+  }
+
+  // Secret key
+  const secretKey = '6LcuJjQUAAAAAHknCKafaonyXFMrIci4g9YFwJ9P';
+
+  // Verify URL
+  const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}&remoteip${req.connection.remoteAddress}`;
+
+  // Make request to verify URL
+  request(verifyUrl, (err, response, body) => {
+    body = JSON.parse(body);
+
+    // if not successful
+    if (body.success !== undefined && !body.success) {
+      return res.json({"success": false, "msg": "Failed captcha verification"});
+    }
+
+    // if successful
+    return res.json({"success": true, "msg": "Captcha passed"});
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
